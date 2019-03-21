c3497ba541b346fcd3ce7b69ec4846f5019578db
Yo-main <arnal.romain@gmail.com>
2019-03-21 23:41:33 +0100

    yo

--- script.py
+++ script.py
@@ -1,16 +1,17 @@
 from itertools import product
+from timeit import time
 
 
 def sudoku_solver(puzzle):
-    print_puzzle(puzzle)
     check_puzzle(puzzle)
     relatives = get_all_relatives()
+
     solve(puzzle, relatives)
     validity = valid(puzzle, relatives)
-    tracking = []
-    tries = []
 
+    tries = []
     if validity != 0:
+        tracking = []
         if not search(puzzle, relatives, tracking, tries):
             raise ValueError("This sudoku could not be solved")
     print(len(tries))
@@ -28,7 +29,7 @@ def print_puzzle(puzzle):
         print(n)
 
 
-def search(puzzle, relatives, tracking, tries, check_tries=False, count=0):
+def search(puzzle, relatives, tracking, tries, check_tries=False):
     mini = get_min_coord(puzzle, relatives, tries, check_tries)
 
     if not mini:
@@ -36,9 +37,6 @@ def search(puzzle, relatives, tracking, tries, check_tries=False, count=0):
 
     values, i, j = mini
     tracking.append((i, j))
-
-    #print("{}{}".format(' '*count, (i, j)))
-    count += 1
 
     for val in values:
         puzzle[i][j] = val
@@ -49,18 +47,18 @@ def search(puzzle, relatives, tracking, tries, check_tries=False, count=0):
         if validity == 0:
             return True
         elif validity == 1:
-            if search(puzzle, relatives, tracking, tries, count):
+            if search(puzzle, relatives, tracking, tries, True):
                 return True
         elif validity == 2: 
             clean_tracking(i, j, puzzle, tracking)
    
     clean_tracking(i, j, puzzle, tracking)
     puzzle[i][j] = 0
-    del tracking[-1]
+    tracking.pop()
 
     while not tracking:
         tries.append((i, j))
-        if search(puzzle, relatives, tracking, tries, True, count):
+        if search(puzzle, relatives, tracking, tries, True):
             return True
 
     return False
@@ -70,7 +68,7 @@ def clean_tracking(i, j, puzzle, tracking):
     while tracking[-1] != (i, j):
         x, y = tracking[-1]
         puzzle[x][y] = 0
-        del tracking[-1]
+        tracking.pop()
 
 
 def get_min_coord(puzzle, relatives, tries, check_tries):
@@ -139,6 +137,7 @@ def cells():
     for i, j in product(range(9), repeat=2):
         yield (i, j)
 
+
 def copy(puzzle):
     out = generate_empty_dic()
     for i, j in cells():
@@ -146,77 +145,24 @@ def copy(puzzle):
     return out
 
 
+def parse_input(raw):
+    raw = raw[:-1]
+    assert len(raw) == 9*9
+    assert raw.isdigit()
+    out = generate_empty_dic()
+    for i, j in product(range(9), repeat=2):
+        out[i][j] = int(raw[i*9+j])
+    return out
+
 if __name__ == "__main__":
-    hard = [[0, 0, 6, 1, 0, 0, 0, 0, 8], 
-            [0, 8, 0, 0, 9, 0, 0, 3, 0], 
-            [2, 0, 0, 0, 0, 5, 4, 0, 0], 
-            [4, 0, 0, 0, 0, 1, 8, 0, 0], 
-            [0, 3, 0, 0, 7, 0, 0, 4, 0], 
-            [0, 0, 7, 9, 0, 0, 0, 0, 3], 
-            [0, 0, 8, 4, 0, 0, 0, 0, 6], 
-            [0, 2, 0, 0, 5, 0, 0, 8, 0], 
-            [1, 0, 0, 0, 0, 2, 5, 0, 0]]
 
-    easy = [[0, 0, 0, 4, 0, 8, 1, 5, 0], 
-            [1, 0, 5, 6, 0, 3, 2, 0, 0], 
-            [0, 0, 0, 9, 1, 5, 7, 0, 0], 
-            [2, 5, 6, 0, 0, 1, 9, 0, 4], 
-            [0, 9, 0, 0, 0, 0, 0, 2, 0], 
-            [4, 0, 3, 8, 0, 0, 6, 1, 5], 
-            [0, 0, 8, 3, 4, 9, 0, 0, 0], 
-            [0, 0, 9, 2, 0, 6, 4, 0, 3], 
-            [0, 6, 4, 1, 0, 7, 0, 0, 0]]
-    
-    issue = [[0, 0, 6, 3, 0, 0, 0, 0, 2],
-             [0, 3, 0, 0, 4, 0, 0, 6, 0],
-             [7, 0, 0, 0, 0, 1, 9, 0, 0],
-             [2, 0, 0, 0, 0, 8, 7, 0, 0],
-             [0, 1, 0, 0, 5, 0, 0, 4, 0],
-             [0, 0, 9, 1, 0, 0, 0, 0, 5],
-             [0, 0, 7, 4, 0, 0, 0, 0, 8],
-             [0, 9, 0, 0, 1, 0, 0, 2, 0],
-             [3, 0, 0, 0, 0, 5, 6, 0, 0]]
+    with open("input.txt") as file_:
+        for raw in file_:
+            puzzle = parse_input(raw)
+            start = time.time()
+            solved = sudoku_solver(puzzle)
+            end = time.time()
+            print("Solved in {:0.5f} seconds".format(end-start))
+            print_puzzle(solved)
+            print()
 
-
-    issue2 = [[6, 0, 0, 0, 0, 0, 0, 0, 2],
-              [0, 0, 3, 6, 0, 1, 7, 0, 0],
-              [0, 7, 0, 0, 4, 0, 0, 1, 0],
-              [0, 5, 0, 9, 0, 4, 0, 3, 0],
-              [0, 0, 9, 0, 0, 0, 1, 0, 0],
-              [0, 6, 0, 7, 0, 8, 0, 2, 0],
-              [0, 3, 0, 0, 6, 0, 0, 5, 0],
-              [0, 0, 5, 3, 0, 9, 4, 0, 0],
-              [7, 0, 0, 0, 0, 0, 0, 0, 3]]
-
-    issue3 = [[0, 9, 0, 0, 7, 1, 0, 0, 4],
-              [2, 0, 0, 0, 0, 0, 0, 7, 0],
-              [0, 0, 3, 0, 0, 0, 2, 0, 0],
-              [0, 0, 0, 9, 0, 0, 0, 3, 5],
-              [0, 0, 0, 0, 1, 0, 0, 8, 0],
-              [7, 0, 0, 0, 0, 8, 4, 0, 0],
-              [0, 0, 9, 0, 0, 6, 0, 0, 0],
-              [0, 1, 7, 8, 0, 0, 0, 0, 0],
-              [6, 0, 0, 0, 2, 0, 7, 0, 0]]
-
-    issue4 = [[0, 0, 2, 0, 0, 1, 0, 0, 0],
-              [3, 0, 0, 6, 8, 0, 7, 0, 0],
-              [0, 0, 0, 5, 0, 0, 6, 0, 0],
-              [0, 0, 7, 0, 0, 4, 0, 0, 0],
-              [0, 0, 4, 0, 1, 7, 0, 0, 9],
-              [0, 0, 0, 9, 0, 0, 8, 0, 0],
-              [7, 0, 5, 0, 2, 0, 0, 3, 0],
-              [0, 1, 0, 0, 0, 0, 0, 5, 0],
-              [0, 8, 0, 0, 4, 0, 9, 0, 1]]
-
-    issue5 = [[0, 0, 0, 0, 9, 0, 5, 0, 3],
-              [0, 4, 5, 0, 0, 0, 0, 0, 1],
-              [0, 0, 0, 0, 0, 6, 4, 0, 0],
-              [0, 0, 9, 0, 0, 2, 0, 0, 0],
-              [0, 1, 0, 9, 8, 4, 0, 6, 0],
-              [0, 0, 0, 5, 0, 0, 2, 0, 0],
-              [0, 0, 1, 8, 0, 0, 0, 0, 0],
-              [2, 0, 0, 0, 0, 0, 1, 8, 0],
-              [8, 0, 3, 0, 7, 0, 0, 0, 0]]
-
-    solved = sudoku_solver(issue5)
-    print_puzzle(solved)
